ac6a2bd709362b38fbbeb0f8cdd3c645d30e9e89
Adrian Vovk <adrianvovk@gmail.com>
2019-11-04 16:42:09 -0500

    Fixup

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/api/Util.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/api/Util.kt
@@ -1,24 +1,90 @@
 package org.firstinspires.ftc.teamcode.api
 
+import com.acmerobotics.roadrunner.trajectory.TrajectoryConfig
+import com.fasterxml.jackson.core.util.VersionUtil
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.qualcomm.hardware.lynx.LynxModule
 import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.DcMotorEx
 import com.qualcomm.robotcore.hardware.DcMotorSimple
+import com.qualcomm.robotcore.hardware.HardwareMap
+import com.qualcomm.robotcore.util.Version
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil
+import org.openftc.revextensions2.ExpansionHubMotor
+import java.lang.NumberFormatException
+import java.lang.RuntimeException
 
-data class Point(var x: Double = 0.0, var y: Double = 0.0) {
-    operator fun plus(other: Point) = Point(this.x + other.x, this.y + other.y)
+
+object TrajectoryLoader {
+    val MAPPER = ObjectMapper(YAMLFactory())
+
+    init {
+        MAPPER.registerModule(KotlinModule())
+    }
+
+    fun loadConfig(name: String): TrajectoryConfig {
+        val input = AppUtil.getDefContext().assets.open("trajectory/" + name + ".yaml")
+        return MAPPER.readValue(input, TrajectoryConfig::class.java)
+    }
+
+    /**
+     * Load trajectory from config file by name
+     */
+    fun load(name: String) = loadConfig(name).toTrajectory()
 }
 
-typealias P = Point // So you can say `position = P(y = 20.0)`
 
-@DslMarker
-annotation class RobotAPI
+object LynxModuleUtil {
+    class LynxFirmwareVersionException(msg: String) : RuntimeException(msg)
 
-fun DcMotor.init(reverse: Boolean) = (this as DcMotorEx).init(reverse)
+    val MIN_VERSION = LynxFwVersion(1, 8, 2)
 
-fun DcMotorEx.init(reverse: Boolean) = apply {
+    data class LynxFwVersion(val major: Int, val minor: Int, val patch: Int) : Comparable<LynxFwVersion> {
+
+        override fun toString() = "%d.%d.%d".format(major, minor, patch)
+
+        override operator fun compareTo(other: LynxFwVersion): Int {
+            var cmp = major.compareTo(other.major)
+            if (cmp != 0) return cmp
+            cmp = minor.compareTo(other.minor)
+            if (cmp != 0) return cmp
+            return patch.compareTo(other.patch)
+        }
+    }
+
+    fun getFwVersion(module: LynxModule): LynxFwVersion? {
+        val verStr = module.nullableFirmwareVersionString ?: return null
+        val parts = verStr.split("[ :,]+")
+        try {
+            return LynxFwVersion(parts[3].toInt(), parts[5].toInt(), parts[7].toInt())
+        } catch (e: NumberFormatException) {
+            return null
+        }
+    }
+
+    fun ensureMinFWVersion(hardwareMap: HardwareMap) {
+        var outdated = mutableMapOf<String, LynxFwVersion?>()
+        for (module in hardwareMap.getAll(LynxModule::class.java)) {
+            val version = getFwVersion(module)
+            if (version == null || version < MIN_VERSION)
+                for (name in hardwareMap.getNamesOf(module))
+                    outdated.put(name, version)
+        }
+        if (outdated.isNotEmpty()) {
+            val builder = StringBuilder()
+            builder.appendln("One or more REV Hubs has outdated firmware")
+            builder.appendln("Required version: %s".format(MIN_VERSION.toString()))
+            for ((name, version) in outdated)
+                builder.appendln("%s: %s".format(name, version?.toString() ?: "Unknown"))
+            throw LynxFirmwareVersionException(builder.toString())
+        }
+    }
+}
+
+fun ExpansionHubMotor.init(reverse: Boolean = false) = this.apply {
     direction = if (reverse) DcMotorSimple.Direction.REVERSE else DcMotorSimple.Direction.FORWARD
+    mode = DcMotor.RunMode.RUN_USING_ENCODER
     zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
-    power = 0.0
-    //setPositionPIDFCoefficients(p)
-    //setVelocityPIDFCoefficients(p,i,d,f)
 }
